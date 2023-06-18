5a34b9dd659877479630cb63c9c847c57dcfc860
Artak Kirakosyan <artakkirakosyan96@mail.ru>
2023-06-18 19:53:45 +0400

    Add euro into the currency list

--- parser/currency_converter.py
+++ parser/currency_converter.py
@@ -1,23 +1,28 @@
 amd = "AMD"
 us = "US"
 cad = "CA"
-usd_amd = 499
-cad_amd = 387
+eur = "€"
+usd_amd = 389
+cad_amd = 291
+eur_amd = 423
 exchange = {
     amd: {
         amd: 1,
         us: 1 / usd_amd,
         cad: 1 / cad_amd,
+        eur: 1 / eur_amd,
     },
     us: {
         amd: usd_amd,
         us: 1,
         cad: usd_amd / cad_amd,
+        eur: usd_amd / eur_amd
     },
     cad: {
         amd: cad_amd,
         us: cad_amd / usd_amd,
         cad: 1,
+        eur: cad_amd / eur_amd
     }
 
 }
