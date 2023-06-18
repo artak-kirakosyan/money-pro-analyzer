b0cf4e829a681196829c9393bacca8c736de05ac
Artak Kirakosyan <artakkirakosyan96@mail.ru>
2023-06-18 21:14:27 +0400

    Fill in empty gaps

--- money_pro_plot/plot_helper.py
+++ money_pro_plot/plot_helper.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple, Any
+
 from matplotlib import pyplot as plt
 
 from parser.statistics import CSVParser
@@ -9,9 +11,28 @@ def get_plot_figure(data: CSVParser, category, subcategory):
     )
     dates = list(subcategory_data.keys())
     amounts = list(subcategory_data.values())
-    dates = ["{}/{}".format(i[1], i[0]) for i in dates]
+    filled_dates, filled_amounts = fill_in_empty_dates(dates, amounts)
+    plt_dates = ["{}/{}".format(i[1], i[0]) for i in filled_dates]
     fig = plt.figure(figsize=(15, 9))
-    plt.plot(dates, amounts)
+    plt.plot(plt_dates, filled_amounts)
     plt.grid()
     plt.xticks(rotation=60)
     return fig
+
+
+def fill_in_empty_dates(dates: List[Tuple[int, int]], amounts: List[Any]):
+    # TODO fix me idiot
+    years = [i[0] for i in dates]
+    min_year = min(years)
+    max_year = max(years)
+    filled_dates = []
+    filled_amounts = []
+    for year in range(min_year, max_year + 1):
+        for month in range(1, 13):
+            if (year, month) in dates:
+                filled_amounts.append(amounts[dates.index((year, month))])
+            else:
+                filled_amounts.append(0)
+            filled_dates.append((year, month))
+
+    return filled_dates, filled_amounts
